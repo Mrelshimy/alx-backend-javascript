580e9b795eb24f6cdc7b11ab211c8a4c53a16ab4
Mrelshimy <mraafat.elsayed@gmail.com>
2024-05-28 22:10:50 +0300

    task 12

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
