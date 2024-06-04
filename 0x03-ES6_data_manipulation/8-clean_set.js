ecea664fe0059b8ec44234d7a790ee36cc927271
Mrelshimy <mraafat.elsayed@gmail.com>
2024-06-04 23:31:42 +0300

    bugfix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,12 @@
 export default function cleanSet(set, startString) {
   if (startString === '') return '';
   let newStr = '';
-  Array.from(set).forEach((element) => {
-    if (element.startsWith(startString)) {
-      newStr += `${element.slice(startString.length)}-`;
-    }
-  });
+  if (set instanceof Set && typeof startString === 'string') {
+    Array.from(set).forEach((element) => {
+      if (element.startsWith(startString)) {
+        newStr += `${element.slice(startString.length)}-`;
+      }
+    });
+  }
   return newStr.slice(0, -1);
 }
